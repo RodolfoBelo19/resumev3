ecc589774e50fcd69dd7c767412bb93123928b9a
Rodolfo Belo <64740047+RodolfoBelo19@users.noreply.github.com>
2023-04-20 01:11:43 -0300

    add theme more-about-me

--- src/presentation/components/MoreAboutMe/index.tsx
+++ src/presentation/components/MoreAboutMe/index.tsx
@@ -2,11 +2,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay } from "swiper";
 
 import "swiper/swiper-bundle.css";
-import Link from "next/link";
+import { useThemeValue } from "@/presentation/contexts/ThemeContext";
 
 SwiperCore.use([Autoplay]);
 
 export const MoreAboutMe = () => {
+  const theme = useThemeValue();
+
   return (
     <div className="mx-auto md:py-12 py-5 max-w-xl px-2 sm:px-6 lg:px-8">
       <h1 className="px-5 text-xl text-center">Mais sobre mim...</h1>
@@ -19,18 +21,50 @@ export const MoreAboutMe = () => {
           spaceBetween={50}
           slidesPerView={1}
         >
-          <SwiperSlide className="bg-zinc-900 rounded-md p-5 cursor-pointer">
+          <SwiperSlide
+            className={`rounded-md p-5 cursor-pointer
+            ${
+              !theme
+                ? "text-zinc-400 bg-zinc-900"
+                : "text-black bg-white bg-opacity-50"
+            }
+          `}
+          >
             Hobbies: Entusiasta em tecnologia, livros, games, filmes e séries.
           </SwiperSlide>
-          <SwiperSlide className="bg-zinc-900 rounded-md p-5 cursor-pointer">
+          <SwiperSlide
+            className={`rounded-md p-5 cursor-pointer
+            ${
+              !theme
+                ? "text-zinc-400 bg-zinc-900"
+                : "text-black bg-white bg-opacity-50"
+            }
+          `}
+          >
             Qualidades: Comunicativo, Simpático, Curioso, Trabalho bem em equipe
             e Competência técnica.
           </SwiperSlide>
-          <SwiperSlide className="bg-zinc-900 rounded-md p-5 cursor-pointer">
+          <SwiperSlide
+            className={`rounded-md p-5 cursor-pointer
+            ${
+              !theme
+                ? "text-zinc-400 bg-zinc-900"
+                : "text-black bg-white bg-opacity-50"
+            }
+          `}
+          >
             Novidades: A fim de melhorar meu conhecimento profissional, estou me
             dedicando ao domínio do inglês.
           </SwiperSlide>
-          <SwiperSlide className="bg-zinc-900 rounded-md p-5 cursor-pointer">
+          <SwiperSlide
+            className={`rounded-md p-5 cursor-pointer
+            ${
+              !theme
+                ? "text-zinc-400 bg-zinc-900"
+                : "text-black bg-white bg-opacity-50"
+            }
+          `}
+          >
             O que me move: Ter uma sólida carreira profissional, para garantir
             estabilidade e confiança, para qual darei um futuro melhor para
             minha família.
