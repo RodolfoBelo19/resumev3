963beaf8a3f4816b99f1efa08cbb043b8f10321e
Rodolfo Belo <64740047+RodolfoBelo19@users.noreply.github.com>
2023-04-20 01:46:14 -0300

    fix contact wpp

--- src/presentation/components/Main/index.tsx
+++ src/presentation/components/Main/index.tsx
@@ -3,10 +3,11 @@ import profile from "../../../../public/profile.jpg";
 import { ImageOrVideoBackground } from "../ImageOrVideoBackground";
 import { Social } from "../Social";
 import { useThemeValue } from "@/presentation/contexts/ThemeContext";
+import Link from "next/link";
 
 export const Main = () => {
   const theme = useThemeValue();
-  
+
   return (
     <>
       <div className="pt-20 mx-auto max-w-7xl h-screen px-2 sm:px-6 lg:px-8">
@@ -21,14 +22,29 @@ export const Main = () => {
             Rodolfo Belo
           </p>
           <div className="flex gap-2">
-            <button className={`${!theme ? "bg-cyan-600 text-white shadow-md" : "bg-white text-black shadow-md"} 
-            font-thin p-1 rounded-md w-40`}>
-              Contrate-me
-            </button>
-            <button className={`${!theme ? "bg-cyan-600 text-white shadow-md" : "bg-white text-black shadow-md"} 
-            font-thin p-1 rounded-md w-40`}>
+            <Link
+              href="https://wa.me/5521997929884"
+              className={`${
+                !theme
+                  ? "bg-cyan-600 text-white shadow-md"
+                  : "bg-white text-black shadow-md"
+              } 
+              font-thin text-center p-1 rounded-md w-40`}
+            >
+              Contate-me
+            </Link>
+            <Link
+              href="https://drive.google.com/file/d/1oBVze0__j353SiAmw6uIoKzwRt_vqbkU/view?usp=sharing"
+              target="_blank"
+              className={`${
+                !theme
+                  ? "bg-cyan-600 text-white shadow-md"
+                  : "bg-white text-black shadow-md"
+              } 
+              font-thin text-center p-1 rounded-md w-40`}
+            >
               Download CV
-            </button>
+            </Link>
           </div>
           <Social classProp="py-16 flex gap-3" />
         </div>
