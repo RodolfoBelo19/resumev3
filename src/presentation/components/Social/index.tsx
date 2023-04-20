d4deb93a5851b76a1c0f4b7b25469e366de17cdf
Rodolfo Belo <64740047+RodolfoBelo19@users.noreply.github.com>
2023-04-20 01:35:22 -0300

    fix contact

--- src/presentation/components/Social/index.tsx
+++ src/presentation/components/Social/index.tsx
@@ -11,17 +11,26 @@ type SocialProps = {
 
 export const Social = ({ classProp }: SocialProps) => {
   const theme = useThemeValue();
-  
+
   return (
     <div className={classProp}>
-      <Link href="" className="hover:bg-sky-800 rounded-sm">
+      <Link
+        target="_blank"
+        href="https://www.linkedin.com/in/rodolfo-belo-5786b21a0"
+        className="hover:bg-sky-800 rounded-sm"
+      >
         <FaLinkedin className="text-white cursor-pointer text-4xl" />
       </Link>
-      <Link href="" className="hover:bg-zinc-700 rounded-full">
+      <Link
+        target="_blank"
+        href="https://github.com/RodolfoBelo19"
+        className="hover:bg-zinc-700 rounded-full"
+      >
         <FaGithub className="text-white cursor-pointer text-4xl" />
       </Link>
       <Link
-        href=""
+        target="_blank"
+        href="https://www.instagram.com/rodolfobeloo"
         className="hover:bg-gradient-to-r from-purple-500 via-orange-500 to-pink-500 rounded-xl"
       >
         <FaInstagram className="text-white cursor-pointer text-4xl rounded" />
