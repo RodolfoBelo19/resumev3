15cb73eba398d7785f4ef85d50cfa32349000bf9
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-04-29 12:17:13 -0300

    feat: upload image

---
+++ src/infra/http/axiosHttpClient/index.ts
@@ -0,0 +1,16 @@
+import axios, {AxiosResponse} from "axios";
+import { HttpPostClient } from "@/data/protocols/http/httpPostClient";
+
+export class AxiosHttpClient implements HttpPostClient {
+  async post(params: HttpPostClient.Params): Promise<any> {
+    let axiosResponse: AxiosResponse;
+    try {
+      axiosResponse = await axios.post(params.url, params.body, {
+        headers: params.headers,
+      });
+    } catch (error: any) {
+      axiosResponse = error.response;
+    }
+    return axiosResponse;
+  }
+}
