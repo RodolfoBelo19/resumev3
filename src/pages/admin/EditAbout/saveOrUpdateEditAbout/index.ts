f5f0dd3c8786b022cf7703cd1f072ab1f88971e1
Rodolfo Belo <64740047+RodolfoBelo19@users.noreply.github.com>
2023-05-21 11:41:32 -0300

    fix: saveOrUpdateEditAbout

--- src/pages/admin/EditAbout/saveOrUpdateEditAbout/index.ts
+++ src/pages/admin/EditAbout/saveOrUpdateEditAbout/index.ts
@@ -1,25 +1,30 @@
 import { IAbout } from '@/interfaces/IAbout';
-import { AxiosHttpClient } from "@/infra/http/axiosHttpClient";
-import { toast } from "react-toastify";
+import { AxiosHttpClient } from '../../../../infra/http/axiosHttpClient';
+import { toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 const httpClient = new AxiosHttpClient();
 
-export const saveOrUpdateEditAbout = async (values: IAbout) => {
-  if (values._id) {
-    await httpClient.patch({
-      url: `${process.env.NEXT_PUBLIC_API_URL}/about/${values._id}`,
-      body: values,
-    }).then((response) => {
-      toast.success("Alterado com sucesso!")
-    }).catch((error) => {
-      toast.error("Erro ao alterar!")
-    });
-    return;
-  }
+export const saveOrUpdateEditAbout = async (values: IAbout): Promise<void> => {
+  try {
+    if (values._id) {
+      await httpClient.patch({
+        url: `${process.env.NEXT_PUBLIC_API_URL}/about/${values._id}`,
+        body: values,
+      });
+      toast.success('Alterado com sucesso!');
+      return Promise.resolve();
+    }
 
-  httpClient.post({
-    url: "${process.env.NEXT_PUBLIC_API_URL}/about",
-    body: values,
-  });
+    await httpClient.post({
+      url: `${process.env.NEXT_PUBLIC_API_URL}/about`,
+      body: values,
+    });
+
+    toast.success('Salvo com sucesso!');
+    return Promise.resolve();
+  } catch (error) {
+    toast.error('Erro ao salvar ou alterar!');
+    return Promise.reject(error); // Adicionando um valor de rejeição em caso de erro
+  }
 };
